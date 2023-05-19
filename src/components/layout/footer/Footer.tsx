c5b7f1969b4ac0aa30b635bcb82aad10c0ab0ade
cherryc0ck <qstar9537@naver.com>
2023-05-19 11:09:14 +0900

    :sparkles: 푸터 UI 구현

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,11 +1,85 @@
 "use client";
 
+import Link from "next/link";
+
+import { PATH } from "@/constants";
+import {
+  CroundLogoIcon,
+  FooterFacebookIcon,
+  FooterInstagramIcon,
+  FooterTwiterIcon,
+} from "@/assets/icons";
 import * as S from "./styled";
+
+const SERVICE = [
+  {
+    label: "크리에이터 목록",
+    path: PATH.CREATORS,
+  },
+  {
+    label: "숏클래스 목록",
+    path: PATH.SHORTS,
+  },
+  {
+    label: "콘텐츠 목록",
+    path: PATH.CONTENTS,
+  },
+  {
+    label: "크리에이터 등록",
+    path: PATH.CREATORS,
+  },
+];
+const CROUND = [
+  {
+    label: "이용약관",
+    path: PATH.ROOT,
+  },
+  {
+    label: "개인정보처리방침",
+    path: PATH.ROOT,
+  },
+];
 
 export default function Footer() {
   return (
     <S.Footer>
-      <div></div>
+      <S.Container>
+        <S.TopWrapper>
+          <Link href={PATH.ROOT}>
+            <CroundLogoIcon />
+          </Link>
+        </S.TopWrapper>
+        <S.MiddleWrapper>
+          <S.ListWrapper>
+            <h3>서비스</h3>
+            <ul>
+              {SERVICE.map((list) => (
+                <li>
+                  <Link href={list.path}>{list.label}</Link>
+                </li>
+              ))}
+            </ul>
+          </S.ListWrapper>
+          <S.ListWrapper>
+            <h3>크라운드</h3>
+            <ul>
+              {CROUND.map((list) => (
+                <li>
+                  <Link href={list.path}>{list.label}</Link>
+                </li>
+              ))}
+            </ul>
+          </S.ListWrapper>
+        </S.MiddleWrapper>
+        <S.BottomWrapper>
+          <div>
+            <FooterTwiterIcon />
+            <FooterInstagramIcon />
+            <FooterFacebookIcon />
+          </div>
+          <S.Copyright>copyright Cround. All rights reserved.</S.Copyright>
+        </S.BottomWrapper>
+      </S.Container>
     </S.Footer>
   );
 }
--- src/components/layout/footer/styled.ts
+++ src/components/layout/footer/styled.ts
@@ -2,8 +2,83 @@ import styled, { css } from "styled-components";
 
 export const Footer = styled.footer`
   ${({ theme }) => css`
-    width: 100%;
-    height: 430px;
-    background-color: ${theme.colors.neutral800};
+    ${theme.media.mobile} {
+      width: 100%;
+      background-color: ${theme.colors.neutral900};
+    }
+  `};
+`;
+
+export const Container = styled.div`
+  ${({ theme }) => css`
+    ${theme.media.mobile} {
+      padding: 24px 20px 8px 20px;
+    }
+  `};
+`;
+
+export const TopWrapper = styled.div`
+  ${({ theme }) => css`
+    ${theme.media.mobile} {
+      margin-bottom: 24px;
+      padding-bottom: 8px;
+      border-bottom: 1px solid ${theme.colors.neutral800};
+    }
+  `};
+`;
+
+export const MiddleWrapper = styled.div`
+  ${({ theme }) => css`
+    ${theme.media.mobile} {
+      ${theme.flex.col()};
+      gap: 24px;
+      margin-bottom: 40px;
+    }
+  `};
+`;
+
+export const ListWrapper = styled.div`
+  ${({ theme }) => css`
+    ${theme.media.mobile} {
+      ${theme.flex.col()};
+      gap: 16px;
+
+      h3 {
+        ${theme.typo.label.lg};
+        color: ${theme.colors.white};
+      }
+
+      ul {
+        ${theme.flex.col()};
+        gap: 8px;
+      }
+      ul li {
+        ${theme.typo.label.sm};
+        color: ${theme.colors.neutral100};
+      }
+    }
+  `};
+`;
+
+export const BottomWrapper = styled.div`
+  ${({ theme }) => css`
+    ${theme.media.mobile} {
+      ${theme.flex.col()};
+      gap: 24px;
+
+      div {
+        ${theme.flex.row()};
+        gap: 32px;
+      }
+    }
+  `};
+`;
+
+export const Copyright = styled.div`
+  ${({ theme }) => css`
+    ${theme.media.mobile} {
+      ${theme.typo.label.sm};
+      color: ${theme.colors.neutral600};
+    }
   `};
 `;
