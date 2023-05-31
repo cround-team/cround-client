2fcaef3d5ec413ffe7e155f73b21af2b56b32338
cherryc0ck <qstar9537@naver.com>
2023-05-31 20:11:31 +0900

    :sparkles: 이메일, 비밀번호, 이름 유효성 검사 구현 (#27)

---
+++ src/utils/validation.ts
@@ -0,0 +1,53 @@
+export const nameValidCheck = (id: string): boolean => {
+  const regex = /^[가-힣]{2,6}$/;
+  return regex.test(id);
+};
+
+export const emailValidCheck = (email: string): boolean => {
+  const regex = /^[^\s<>]+@[^\s<>]+\.[^\s<>]+$/;
+
+  return regex.test(email);
+};
+
+export const passwordValidCheck = (pw: string): boolean => {
+  const regex =
+    /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/;
+  return regex.test(pw);
+};
+
+export const psw2ValidCheck = (psw1: string, psw2: string): boolean => {
+  return psw1 === psw2;
+};
+
+export const nicknameCheck = (nickname: string): boolean => {
+  const regex = /^[가-힣a-zA-Z0-9]{2,6}$/;
+  return regex.test(nickname);
+};
+
+// export const signupValidCheck = (type: string, value: string) => {
+//   let regex: RegExp | undefined;
+//   switch (type) {
+//     case "name":
+//       regex = /^[가-힣]{1,6}$/;
+//       break;
+
+//     case "email":
+//       regex = /^[^\s<>]+@[^\s<>]+\.[^\s<>]+$/;
+//       break;
+
+//     case "password":
+//       regex =
+//         /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/;
+//       break;
+
+//     case "nickname":
+//       regex = /^[가-힣a-zA-Z0-9]{2,6}$/;
+//       break;
+//     default:
+//       break;
+//   }
+//   if (regex) {
+//     return regex.test(value);
+//   }
+//   return false;
+// };
