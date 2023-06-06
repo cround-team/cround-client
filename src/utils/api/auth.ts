562a000349751e8fbd94f1677e6d86d8babda496
cherryc0ck <qstar9537@naver.com>
2023-06-06 22:06:50 +0900

    :truck: 디렉토리 이동 (#29)

---
+++ src/utils/api/auth.ts
@@ -0,0 +1,62 @@
+import apiInstance from "./server";
+
+type SignupBody = {
+  username: string;
+  nickname: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  interestPlatform: any;
+};
+
+type EmailDupCheckBody = {
+  email: string;
+};
+
+type NicknameDupCheckBody = {
+  nickname: string;
+};
+
+type LoginBody = {
+  email: string;
+  password: string;
+};
+
+export const signupApi = async (body: SignupBody) => {
+  const response = await apiInstance.post("/api/members", body);
+
+  return response;
+};
+
+export const emailDupCheckApi = async (body: EmailDupCheckBody) => {
+  const response = await apiInstance.post(
+    "/api/members/validations/email",
+    body
+  );
+
+  return response;
+};
+
+export const nicknameDupCheckApi = async (body: NicknameDupCheckBody) => {
+  const response = await apiInstance.post(
+    "/api/members/validations/nickname",
+    body
+  );
+
+  return response;
+};
+
+export const loginApi = async (body: LoginBody) => {
+  const response = await apiInstance.post("/auth/login", body);
+
+  return response;
+};
+
+export const kakaoSocialApi = async (params?: Record<string, any>) => {
+  const response = await apiInstance.get(
+    "https://kauth.kakao.com/oauth/authorize",
+    { params }
+  );
+
+  return response;
+};
