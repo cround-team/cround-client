bb9726f3b73df6c9a639b15bc7a59d87aa439236
cherryc0ck <qstar9537@naver.com>
2023-06-07 22:02:38 +0900

    :sparkles: 숏클래스 조회 API 연동 (#32)

---
+++ src/utils/api/short.ts
@@ -0,0 +1,7 @@
+import apiInstance from "./server";
+
+export const shortsApi = async (params: Record<string, any>) => {
+  const response = await apiInstance.get("/api/shorts", { params });
+
+  return response;
+};
