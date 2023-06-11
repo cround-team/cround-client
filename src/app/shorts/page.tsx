c037ccaf9c140364b78eb3787fbc3a864fa476db
cherryc0ck <qstar9537@naver.com>
2023-06-11 19:19:43 +0900

    :sparkles: 숏클래스 목록 페이지 API 연동 (#32)

--- src/app/shorts/page.tsx
+++ src/app/shorts/page.tsx
@@ -1,104 +1,23 @@
 "use client";
 
-import { Options } from "@/components/common/dropdown/sort/SortDropdown";
 import PageFilter from "@/components/common/filter/pageFilter/PageFilter";
 import SearchInput from "@/components/common/input/search/SearchInput";
-import { useEffect, useState } from "react";
-import { useImmer } from "use-immer";
-
-import * as S from "./styled";
 import ShortsList from "@/components/domain/shorts/list/ShortsList";
-import { shortsApi } from "@/utils/api/short";
 import type { Filter, Sorts } from "@/types/service";
+import useListPage from "@/hooks/useListPage";
+import * as S from "./styled";
 
 const OPTIONS: Sorts[] = ["신규순", "북마크순", "좋아요순"];
-const INITIAL_SORT = "latest";
+const LIST_TYPE = "shorts";
 
 export default function ShortsPage() {
-  const [shorts, setShorts] = useImmer({
-    data: [],
-  });
-  const [filter, setFilter] = useImmer<Filter>({
-    platforms: [],
-    sort: INITIAL_SORT,
-  });
-  const [isNextPage, setIsNextPage] = useState(false);
-
-  useEffect(() => {
-    fetchShortsData();
-  }, []);
-
-  const fetchShortsData = (id?: number) => {
-    const getResponse = async () => {
-      try {
-        const params = {
-          sort: filter.sort,
-          filter: filter.platforms.join(","),
-          size: 6,
-          cursorId: id,
-          // 키워드 추가해야함
-          keyword: "",
-        };
-        const response = await shortsApi(params);
-
-        if (id) {
-          setShorts((draft: any) => {
-            draft.data = [...draft.data, ...response.data.pages];
-          });
-        } else {
-          setShorts((draft: any) => {
-            draft.data = [...draft.data, ...response.data.pages];
-          });
-        }
-
-        setIsNextPage(response.data.hasNext);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    getResponse();
-  };
-
-  const fetchFilterData = (
-    sort = filter.sort,
-    platforms = filter.platforms
-  ) => {
-    const getResponse = async () => {
-      try {
-        const params = {
-          sort,
-          size: 6,
-          cursorId: undefined,
-          filter: platforms.join(","),
-          // 키워드 추가해야함
-          keyword: "",
-        };
-        const response = await shortsApi(params);
-        setShorts((draft: any) => {
-          draft.data = response.data.pages;
-        });
-
-        setIsNextPage(response.data.hasNext);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    getResponse();
-  };
-
-  const handleSelectPlatform = (selected: string[]) => {
-    setFilter((draft) => {
-      draft.platforms = selected;
-    });
-    fetchFilterData(filter.sort, selected);
-  };
-
-  const handleSelectSort = (sorted: string) => {
-    setFilter((draft) => {
-      draft.sort = sorted;
-    });
-    fetchFilterData(sorted, filter.platforms);
-  };
+  const {
+    list,
+    isNextPage,
+    handleSelectPlatform,
+    handleSelectSort,
+    fetchListData,
+  } = useListPage({ listType: LIST_TYPE });
 
   return (
     <S.Section>
@@ -108,17 +27,16 @@ export default function ShortsPage() {
         name="search"
         placeholder="플랫폼 또는 관심 정보를 입력해 주세요"
       />
-
       <PageFilter
         options={OPTIONS}
         onSelectSort={handleSelectSort}
         onSubmitPlatform={handleSelectPlatform}
       />
-      {shorts?.data && (
+      {list?.data && (
         <ShortsList
-          data={shorts.data}
+          data={list.data}
           isNextPage={isNextPage}
-          onFetchShortsData={fetchShortsData}
+          onFetchData={fetchListData}
         />
       )}
     </S.Section>
