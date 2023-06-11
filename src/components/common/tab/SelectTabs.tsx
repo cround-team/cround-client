30d1a9361810c905b82092cbc9a54c3dfd5a4681
cherryc0ck <qstar9537@naver.com>
2023-06-11 19:18:00 +0900

    :lipstick: 탭 컵포넌트 스타일링 수정 (#38)

--- src/components/common/tab/SelectTabs.tsx
+++ src/components/common/tab/SelectTabs.tsx
@@ -1,18 +1,20 @@
 import * as S from "./styled";
 
 type SelectTabProps = {
+  className?: string;
   tabs: string[];
   selected: string;
   onTabClick: (selected: string) => void;
 };
 
 export default function SelectTabs({
+  className,
   tabs,
   selected,
   onTabClick,
 }: SelectTabProps) {
   return (
-    <S.ListContainer>
+    <S.ListContainer className={className}>
       {tabs.map((tab, idx) => (
         <S.List key={tab} aria-selected={tab === selected ? "true" : "false"}>
           <button onClick={() => onTabClick(tab)}>{tab}</button>
