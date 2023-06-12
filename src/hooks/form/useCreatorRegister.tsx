dac0a2d429ec5b682b0ebb3caef5d58c37af3c8e
cherryc0ck <qstar9537@naver.com>
2023-06-12 21:22:35 +0900

    :sparkles: 크리에이터 등록폼 훅 구현 (#29)

--- src/hooks/form/useCreatorRegister.tsx
+++ src/hooks/form/useCreatorRegister.tsx
@@ -1,65 +1,156 @@
-import { creatorNicknameCheckApi } from "@/utils/api/creator";
-import { hasKey } from "@/utils/form";
-import React, { useState } from "react";
-import { useImmer } from "use-immer";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
-// type InputMessageType =
-//   | "한글, 영어, 또는 숫자로 2글자 이상 6글자 이내"
-//   | "사용 가능한 닉네임입니다"
-//   | "중복된 이메일입니다.";
-
-type Inputs = {
-  nickname: string;
-  desc: string;
-};
+import { useCreatorRegisterContext } from "@/context/CreatorRegisterContext";
+import { PATH } from "@/constants";
+import { creatorCreateApi, creatorNicknameCheckApi } from "@/utils/api/creator";
+import { useUploadImage } from "../useUploadImage";
+import useAddInput from "../input/useAddInput";
 
 export default function useCreatorRegister() {
-  const [inputValues, setInputValues] = useImmer<Inputs>({
-    nickname: "",
-    desc: "",
-  });
-  const [errorMessage, setErrorMessage] = useState("");
-  const [validMessage, setValidMessage] = useState(
-    "한글, 영어, 또는 숫자로 2글자 이상 6글자 이내"
-  );
-  const isDisabled = !!(inputValues.nickname && inputValues.desc);
+  const router = useRouter();
+  const {
+    formData: {
+      nickname,
+      description,
+      platformHeadType,
+      platformHeadTheme,
+      platformUrl,
+      profileImage,
+    },
+    activityPlatforms,
+    tags,
+    handleSetActivityPlatforms,
+    handleSetTags,
+    handleSetFile,
+  } = useCreatorRegisterContext();
 
-  const handleChangeForm = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { value, name } = e.target;
+  const { selectedImage, previewImage, fileInputRef, handleFileChange } =
+    useUploadImage();
 
-    if (!hasKey(inputValues, name)) {
-      throw new Error("is not valid name");
+  const {
+    inputValues: inputTags,
+    handleAddInput: handleAddTag,
+    handleRemoveInput: handleRemoveTag,
+    handleChangeInput: handleChangeTag,
+  } = useAddInput();
+
+  useEffect(() => {
+    if (!nickname || !description || !profileImage) {
+      router.push(PATH.CREATORS.REGISTER.BASE);
+    } else if (!activityPlatforms.length) {
+      router.push(PATH.CREATORS.REGISTER.PLATFORM);
     }
-    setInputValues((draft) => {
-      draft[name] = value;
-    });
-  };
+  }, []);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  useEffect(() => {
+    if (selectedImage !== null) {
+      handleSetFile(selectedImage);
+    }
+  }, [selectedImage]);
+
+  useEffect(() => {
+    if (inputTags.length > 0) {
+      const tags = inputTags.map((tag) => tag.value);
+      handleSetTags(tags);
+    }
+  }, [inputTags]);
+
+  const isBaseDisabled = !(nickname && description && profileImage);
+  const isAdditionDisabled = !(
+    platformHeadType &&
+    platformHeadTheme &&
+    tags.length &&
+    platformUrl
+  );
+
+  const handleSubmitBase = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      const body = {
-        nickname: inputValues.nickname,
-      };
+      const body = { nickname };
       const res = await creatorNicknameCheckApi(body);
       if (res.status === 200) {
-        console.log(res);
+        console.log("res", res);
+        router.push(PATH.CREATORS.REGISTER.PLATFORM);
       }
-      console.log("res", res);
     } catch (error: any) {
       console.log(error);
     }
   };
 
-  return {
-    inputValues,
-    errorMessage,
-    isDisabled,
-    validMessage,
+  const handleSubmitPlatform = (
+    e: React.FormEvent<HTMLFormElement>,
+    selected: string[]
+  ) => {
+    e.preventDefault();
+    handleSetActivityPlatforms(selected);
+    router.push(PATH.CREATORS.REGISTER.ADDITION);
+  };
 
-    handleChangeForm,
-    handleSubmit,
+  const handleSubmitApi = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const formData = new FormData();
+      console.log(
+        nickname,
+        description,
+        platformHeadType,
+        platformHeadTheme,
+        platformUrl,
+        profileImage,
+        tags,
+        activityPlatforms
+      );
+
+      if (profileImage) {
+        formData.append("profileImage", profileImage);
+      }
+
+      const creatorSaveRequest = {
+        nickname,
+        description,
+        platformHeadType,
+        platformHeadTheme,
+        platformUrl,
+        tags,
+        activityPlatforms,
+      };
+      formData.append(
+        "creatorSaveRequest",
+        new Blob([JSON.stringify(creatorSaveRequest)], {
+          type: "application/json",
+        })
+      );
+      const res = await creatorCreateApi(formData);
+      console.log("res", res);
+      if (res.status === 201) {
+        console.log("201", res);
+        router.push(PATH.CREATORS.REGISTER.SUCCESS);
+      }
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
+
+  const handlePrevStep = () => router.back();
+
+  return {
+    isBaseDisabled,
+    isAdditionDisabled,
+    //
+    previewImage,
+    fileInputRef,
+    inputTags,
+    //
+    handleFileChange,
+    handleAddTag,
+    handleChangeTag,
+    handleRemoveTag,
+    //
+    handlePrevStep,
+    //
+    handleSubmitBase,
+    handleSubmitPlatform,
+    handleSubmitApi,
   };
 }
