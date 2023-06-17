0d711883be6119f892988586b064fef47cd21b49
cherryc0ck <qstar9537@naver.com>
2023-06-17 20:04:06 +0900

    :recycle: 회원가입 모달스텝 리팩토링 (#45)

---
+++ src/components/domain/auth/signup/signupBox/SignupBox.tsx
@@ -0,0 +1,36 @@
+import SignupForm from "./form/SignupForm";
+import SignupTerms from "./terms/SignupTerms";
+import useSignupForm from "./hooks/useSignupForm";
+import SignupModal from "./modal/SignupModal";
+import * as S from "./styled";
+import {
+  SignupNicknameStep,
+  SignupPlatformStep,
+  SignupSuccessStep,
+} from "./modal";
+
+export default function SignupBox() {
+  const {
+    isOpenModal,
+    step,
+    getBaseStepProps,
+    getNicknameStepProps,
+    getPlatformStepProps,
+    getSuccessStepProps,
+  } = useSignupForm();
+  return (
+    <S.Container>
+      <SignupForm {...getBaseStepProps()} />
+      <SignupTerms />
+      <SignupModal isMounted={isOpenModal} step={step}>
+        {step === "nickname" && (
+          <SignupNicknameStep {...getNicknameStepProps()} />
+        )}
+        {step === "platform" && (
+          <SignupPlatformStep {...getPlatformStepProps()} />
+        )}
+        {step === "success" && <SignupSuccessStep {...getSuccessStepProps()} />}
+      </SignupModal>
+    </S.Container>
+  );
+}
---
+++ src/components/domain/auth/signup/signupBox/modal/success/styled.ts
@@ -0,0 +1,28 @@
+import styled, { css } from "styled-components";
+
+export const TextWrapper = styled.div`
+  ${({ theme }) => css`
+    ${theme.media.mobile} {
+      text-align: center;
+      h2 {
+        margin-bottom: 4px;
+        ${theme.typo.title.sm};
+        color: ${theme.colors.neutral900};
+      }
+      p {
+        ${theme.typo.label.sm};
+        color: ${theme.colors.neutral500};
+      }
+    }
+  `};
+`;
+
+export const Figure = styled.figure`
+  ${({ theme }) => css`
+    ${theme.media.mobile} {
+      position: relative;
+      width: 258px;
+      height: 258px;
+    }
+  `};
+`;
