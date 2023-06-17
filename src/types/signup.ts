2ee057b573d2d8760c184cf6a48927408b5db9a2
cherryc0ck <qstar9537@naver.com>
2023-06-17 20:09:00 +0900

    :sparkles: 회원가입 관련 타입 추가 (#45)

--- src/types/signup.ts
+++ src/types/signup.ts
@@ -1,1 +1,25 @@
-export type SignupStep = "nickname" | "platforms" | "success";
+export type SignupForm = {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  nickname: string;
+  platforms: string[];
+};
+
+export type SignupValid = {
+  name: boolean;
+  email: boolean;
+  password: boolean;
+  confirmPassword: boolean;
+  nickname: boolean;
+};
+
+export type SignupMessage = {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  nickname: string;
+};
+export type SignupStep = "base" | "nickname" | "platform" | "success";
