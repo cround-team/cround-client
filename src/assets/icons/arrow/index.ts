66a5f42d2a739b18b2f68682b9bb803af6e086f7
cherryc0ck <qstar9537@naver.com>
2023-06-17 19:53:31 +0900

    :truck: 아이콘 디렉토리 구조 변경 (#45)

--- src/assets/icons/arrow/index.ts
+++ src/assets/icons/arrow/index.ts
@@ -1,3 +1,4 @@
 export { default as ChevronRightIcon } from "./chevron-right.svg";
 export { default as ChevronDownIcon } from "./chevron-down.svg";
 export { default as ChevronLeftIcon } from "./chevron-left.svg";
+export { default as ArrowNarrowLeftIcon } from "./arrow-narrow-left.svg";
--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,27 +1,22 @@
+export * from "./arrow";
+export * from "./logo";
+
 export { default as SearchMdIcon } from "@/assets/icons/search-md.svg";
 export { default as XBtnIcon } from "@/assets/icons/x-button.svg";
 export { default as ModalArrowLeftIcon } from "@/assets/icons/modal-arrow-left.svg";
 export { default as EyeOffIcon } from "@/assets/icons/eye-off.svg";
-export { default as GoogleIcon } from "@/assets/icons/google.svg";
-export { default as GoogleMoIcon } from "@/assets/icons/google-mo.svg";
-export { default as KakaoIcon } from "@/assets/icons/kakao.svg";
-export { default as KakaoMoIcon } from "@/assets/icons/kakao-mo.svg";
-export { default as CroundLogoIcon } from "@/assets/icons/cround-logo.svg";
 export { default as BellIcon } from "@/assets/icons/bell.svg";
 export { default as HamburgerIcon } from "@/assets/icons/hamburger.svg";
 export { default as HeartIcon } from "@/assets/icons/heart.svg";
 export { default as BookmarkIcon } from "@/assets/icons/bookmark.svg";
-export { default as FooterTwiterIcon } from "@/assets/icons/footer-twiter.svg";
-export { default as FooterFacebookIcon } from "@/assets/icons/footer-facebook.svg";
-export { default as FooterInstagramIcon } from "@/assets/icons/footer-instagram.svg";
 
 export { default as CheckIcon } from "@/assets/icons/check.svg";
 export { default as SmileIcon } from "@/assets/icons/smile.svg";
 export { default as RatingStarIcon } from "@/assets/icons/ratingStar.svg";
 
-export { default as FilterIcon } from "@/assets/icons/utils/filter.svg";
-export { default as XCloseIcon } from "@/assets/icons/utils/x-close.svg";
-export { default as CameraIcon } from "@/assets/icons/utils/camera.svg";
+export { default as FilterIcon } from "@/assets/icons/filter.svg";
+export { default as XCloseIcon } from "@/assets/icons/x-close.svg";
+export { default as CameraIcon } from "@/assets/icons/camera.svg";
 export { default as SelectBoxIcon } from "@/assets/icons/select-box.svg";
 export { default as PlusIcon } from "@/assets/icons/plus.svg";
 export { default as UploadCloudIcon } from "@/assets/icons/upload-cloud.svg";
