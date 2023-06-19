9e19d159e97b1222bf4122e0e188956d339c96c1
cherryc0ck <qstar9537@naver.com>
2023-06-19 18:37:32 +0900

    :adhesive_bandage: 각 디렉토리 index 추가 (#39)

---
+++ src/components/domain/contents/index.ts
@@ -0,0 +1,4 @@
+export { default as ContentList } from "./list/ContentList";
+export { default as ContentRegisterBase } from "./register/base/ContentRegisterBase";
+export { default as ContentRegisterPlatform } from "./register/platform/ContentRegisterPlatform";
+export { default as ContentRegisterSuccess } from "./register/success/ContentRegisterSuccess";
---
+++ src/components/domain/home/index.ts
@@ -0,0 +1,2 @@
+export { default as HomePageContainer } from "./container/HomePageContainer";
+export { default as Banner } from "./banner/Banner";
