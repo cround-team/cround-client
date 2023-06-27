8854b4790eb8043d8f06d49af63c66bc5e08c5ce
cherryc0ck <qstar9537@naver.com>
2023-06-27 13:51:42 +0900

    :fire: panel 컴포넌트 삭제 (#57)

--- src/components/common/panel/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AuthPannel } from "./auth/AuthPannel";
-export { default as CreatorDetaulPannel } from "./creatorDetail/CreatorDetailPannel";
-export { default as BookmarkPannel } from "./bookmark/BookmarkPannel";
