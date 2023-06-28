e7a3e4da2afd42d232a515f25d36ef57de7295e5
cherryc0ck <qstar9537@naver.com>
2023-06-28 13:41:00 +0900

    :sparkles: 콘텐츠, 크리에이터, 숏클래스 경로 추가 (#67)

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -18,26 +18,33 @@ export const PATH = {
     LIST: "/creators",
     DETAIL: "/creators/detail",
     REGISTER: "/creators/register",
+    EDIT: "/creators/edit",
   },
   SHORTS: {
     LIST: "/shorts",
     DETAIL: "/shorts/detail",
     REGISTER: "/shorts/register",
+    EDIT: "/shorts/edit",
   },
   CONTENTS: {
     LIST: "/contents",
     DETAIL: "/contents/detail",
     REGISTER: "/contents/register",
+    EDIT: "/contents/edit",
   },
 };
 
 export const PATH_TITLE: Record<string, string> = {
   "/creators/detail": "크리에이터 소개",
   "/creators/register": "크리에이터 등록",
+  "/creators/edit": "개인정보 변경",
   "/shorts/detail": "숏클래스",
   "/shorts/register": "숏클래스 등록",
+  "/shorts/edit": "숏클래스 수정",
   "/contents/detail": "콘텐츠",
   "/contents/register": "콘텐츠 등록",
+  "/contents/edit": "콘텐츠 수정",
   "/mypage/bookmark": "나의 북마크",
   "/mypage/follow": "나의 팔로우",
+  "/mypage/settings": "멘티 개인정보 변경",
 };
