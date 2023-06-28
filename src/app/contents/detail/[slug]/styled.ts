657b9166d01a8cec05978a0aa4446903a5a759aa
cherryc0ck <qstar9537@naver.com>
2023-06-28 13:42:43 +0900

    :sparkles: feat: 콘텐트 상세 페이지 구현 (#67)

--- src/app/contents/detail/[slug]/styled.ts
+++ src/app/contents/detail/[slug]/styled.ts
@@ -9,17 +9,38 @@ export const Section = styled.section`
     }
   `};
 `;
+export const Title = styled.div`
+  ${({ theme }) => css`
+    ${theme.media.mobile} {
+      position: relative;
+      ${theme.flex.row("space-between")};
+      width: 100%;
+      margin-bottom: 8px;
+      word-break: break-word;
+      span {
+        ${theme.typo.body.md};
+        color: ${theme.colors.neutral800};
+      }
+
+      button {
+        width: 20px;
+        height: 20px;
+        svg {
+          width: 100%;
+          height: 100%;
+          stroke: ${theme.colors.neutral900};
+        }
+      }
+    }
+  `};
+`;
 
 export const TextWrapper = styled.div`
   ${({ theme }) => css`
     ${theme.media.mobile} {
       ${theme.flex.col("center", "flex-start")};
-
-      span {
-        margin-bottom: 8px;
-        ${theme.typo.body.md};
-        color: ${theme.colors.neutral800};
-      }
+      width: 100%;
+      word-break: break-word;
 
       h3 {
         margin-bottom: 16px;
@@ -27,7 +48,8 @@ export const TextWrapper = styled.div`
         color: ${theme.colors.neutral900};
       }
 
-      pre {
+      p {
+        white-space: pre-wrap;
         margin-bottom: 24px;
         ${theme.typo.body.md};
         color: ${theme.colors.neutral900};
---
+++ src/app/contents/edit/[slug]/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type ContentEditPageProps = {
+  params: {
+    slug: number;
+  };
+};
+
+export default function ContentEditPage({ params }: ContentEditPageProps) {
+  return (
+    <div>
+      <div>Edit</div>
+    </div>
+  );
+}
