713eb1e4bad0707ebceba569697c6ecd542510bd
cherryc0ck <qstar9537@naver.com>
2023-07-11 22:06:08 +0900

    :lipstick: 멘티 개인정보 변경 페이지 반응형 적용 (#79)

--- src/app/mypage/edit/styled.ts
+++ src/app/mypage/edit/styled.ts
@@ -2,8 +2,14 @@ import styled, { css } from "styled-components";
 
 export const Section = styled.section`
   ${({ theme }) => css`
-    margin: 32px 0 64px 0;
+    max-width: 590px;
+    margin: 32px auto 64px auto;
     padding: 0 20px;
+
+    ${theme.media.desktop} {
+      margin: 40px auto 80px auto;
+      padding: 0;
+    }
   `};
 `;
 
@@ -11,6 +17,11 @@ export const Form = styled.form`
   ${({ theme }) => css`
     ${theme.flex.col()};
     gap: 32px;
+    width: 100%;
+
+    ${theme.media.desktop} {
+      gap: 40px;
+    }
   `};
 `;
 
@@ -18,18 +29,31 @@ export const PlatformWrapper = styled.div`
   ${({ theme }) => css`
     ${theme.flex.col("center", "flex-start")};
     gap: 8px;
-    margin-bottom: 8px;
+
+    ${theme.media.desktop} {
+      gap: 16px;
+    }
   `};
 `;
 
 export const PlatformList = styled.div`
   ${({ theme }) => css`
-    ${theme.flex.row()};
+    /* ${theme.flex.row("space-between")};
     flex-wrap: wrap;
     row-gap: 16px;
     column-gap: 15.5px;
     padding: 16px;
     border: 1px solid ${theme.colors.neutral50};
-    border-radius: ${theme.radius.md};
+    border-radius: ${theme.radius.md}; */
+
+    ${theme.flex.row("space-between")};
+    flex-wrap: wrap;
+    row-gap: 8px;
+    column-gap: 32.5px;
+
+    ${theme.media.desktop} {
+      row-gap: 16px;
+      column-gap: 16px;
+    }
   `};
 `;
