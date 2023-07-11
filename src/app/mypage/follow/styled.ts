eb86eafdeb8b9da7192129fa6b28472d61f09fdb
cherryc0ck <qstar9537@naver.com>
2023-07-11 22:06:32 +0900

    :lipstick: 나의 팔로우 페이지 반응형 적용 (#79)

--- src/app/mypage/follow/styled.ts
+++ src/app/mypage/follow/styled.ts
@@ -5,5 +5,11 @@ export const Section = styled.section`
     margin-top: 16px;
     margin-bottom: 64px;
     padding: 0 20px;
+
+    ${theme.media.desktop} {
+      width: 1200px;
+      margin: 40px auto 80px auto;
+      padding: 0;
+    }
   `};
 `;
