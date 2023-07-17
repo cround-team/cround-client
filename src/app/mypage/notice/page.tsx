d52509b0e6bfebf57774b08c02a7f6a4740a1dc9
cherryc0ck <qstar9537@naver.com>
2023-07-17 21:39:11 +0900

    :sparkles: 알림 페이지 추가 (#81)

---
+++ src/app/mypage/notice/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import * as S from "./styled";
+
+export default function MyNoticePage() {
+  return (
+    <S.Section>
+      <ul>
+        <li></li>
+      </ul>
+    </S.Section>
+  );
+}
