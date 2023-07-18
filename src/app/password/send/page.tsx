ca0f7a0624e4ac4976aa26957d79bb394aba3ddd
cherryc0ck <qstar9537@naver.com>
2023-07-18 21:16:43 +0900

    :sparkles: 비밀번호 이메일 전송 페이지 구현 (#86)

--- src/app/password/send/page.tsx
+++ src/app/password/send/page.tsx
@@ -4,15 +4,22 @@ import { PATH } from "@/constants";
 import { useDeviceType } from "@/hooks";
 import { LinkButton } from "@/components/common";
 import * as S from "./styled";
+import { useEffect, useState } from "react";
 
 export default function PasswordSendPage() {
   const { isDesktop } = useDeviceType();
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    let email: any = new URL(window.location.href).searchParams.get("email");
+    setEmail(email);
+  }, []);
 
   return (
     <S.Container>
       <S.TextWrapper>
         <h2>비밀번호 설정 메일 전송</h2>
-        <span>cround@naver.com</span>
+        <span>{email}</span>
         <p>
           위 주소로 비밀번호 재설정 링크가 전송되었습니다.
           <br />
