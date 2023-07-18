df0ad46d23ec97a3459b0ced35405ef31cf8f4a7
cherryc0ck <qstar9537@naver.com>
2023-07-18 20:03:47 +0900

    :sparkles: 질문 리스트 컴포넌트 UI 구현 (#82)

---
+++ src/components/common/list/asked/creator/styled.ts
@@ -0,0 +1,9 @@
+import styled, { css } from "styled-components";
+
+export const Container = styled.div`
+  ${({ theme }) => css``};
+`;
+
+export const Figure = styled.figure`
+  ${({ theme }) => css``};
+`;
---
+++ src/components/common/list/asked/mentee/styled.ts
@@ -0,0 +1,92 @@
+import styled, { css } from "styled-components";
+
+export const Container = styled.div`
+  ${({ theme }) => css`
+    ${theme.flex.row("flex-start", "flex-start")};
+    gap: 16px;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 16px 44px 16px 20px;
+    border-bottom: 1px solid ${theme.colors.neutral50};
+
+    ${theme.media.desktop} {
+      gap: 16px;
+      padding: 24px 64px 24px 16px;
+    }
+  `};
+`;
+
+export const Figure = styled.figure`
+  ${({ theme }) => css`
+    position: relative;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+
+    image {
+      border-radius: 50%;
+    }
+
+    ${theme.media.desktop} {
+      width: 68px;
+      height: 68px;
+    }
+  `};
+`;
+
+export const TextWrapper = styled.div`
+  ${({ theme }) => css`
+    ${theme.flex.col("center", "flex-start")};
+    flex: 1;
+  `};
+`;
+
+export const CreatorInfo = styled.div`
+  ${({ theme }) => css`
+    ${theme.flex.row()};
+    gap: 8px;
+    margin-bottom: 4px;
+    h3 {
+      ${theme.typo.title.sm};
+      color: ${theme.colors.neutral900};
+    }
+    span {
+      ${theme.typo.label.sm};
+      color: ${theme.colors.neutral700};
+    }
+
+    ${theme.media.desktop} {
+      h3 {
+        ${theme.typo.title.md};
+      }
+      span {
+        ${theme.typo.label.md};
+      }
+    }
+  `};
+`;
+
+export const Text = styled.p`
+  ${({ theme }) => css`
+    margin-bottom: 8px;
+    ${theme.typo.body.sm};
+    color: ${theme.colors.black};
+    ${theme.text.skip(1)};
+
+    ${theme.media.desktop} {
+      ${theme.typo.body.md};
+      ${theme.text.skip(2)};
+    }
+  `};
+`;
+
+export const Time = styled.span`
+  ${({ theme }) => css`
+    ${theme.typo.body.sm};
+    color: ${theme.colors.neutral500};
+
+    ${theme.media.desktop} {
+      ${theme.typo.body.sm};
+    }
+  `};
+`;
