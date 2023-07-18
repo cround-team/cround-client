a1eb6a47045b1880e432bb27bfb924eafeda9a63
cherryc0ck <qstar9537@naver.com>
2023-07-18 21:16:15 +0900

    :sparkles: 비밀번호 변경 페이지 UI 구현 (#86)

--- src/app/password/new/page.tsx
+++ src/app/password/new/page.tsx
@@ -1,33 +1,12 @@
 "use client";
 
-import { useImmer } from "use-immer";
-import { useState } from "react";
-
 import { Button, PasswordInput } from "@/components/common";
+import useNewPassword from "@/components/domain/password/hooks/useNewPassword";
 import * as S from "./styled";
-import { VALID_MSG } from "@/constants";
 
 export default function PasswordNewPage() {
-  const [user, setUser] = useImmer({
-    password: "",
-    confirmPassword: "",
-  });
-  const [inputMessage, setInputMessage] = useState("");
-  const [invalid, setInvalid] = useState(true);
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-  };
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-
-    if (name === "password" || name === "confirmPassword") {
-      setUser((draft) => {
-        draft[name] = value;
-      });
-    }
-  };
+  const { user, inputMessage, invalid, handleChange, handleSubmit } =
+    useNewPassword();
   return (
     <S.Container>
       <S.Title>비밀번호를 변경해주세요.</S.Title>
@@ -41,7 +20,7 @@ export default function PasswordNewPage() {
             id="password"
             name="password"
             placeholder="비밀번호"
-            onChange={handleInputChange}
+            onChange={handleChange}
           />
           <PasswordInput
             isVisibleLabel={user.confirmPassword.length > 0 ? true : false}
@@ -50,8 +29,8 @@ export default function PasswordNewPage() {
             id="confirmPassword"
             name="confirmPassword"
             placeholder="비밀번호를 확인해 주세요"
-            onChange={handleInputChange}
-            inputMessage={VALID_MSG.PSW_TIP}
+            onChange={handleChange}
+            inputMessage={inputMessage}
           />
         </S.InputContainer>
         <Button label="비밀번호 변경" size="56" variant="primary" />
