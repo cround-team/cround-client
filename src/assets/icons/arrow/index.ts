a2015a91c7c969ddeb1b775534723f9af2691b70
cherryc0ck <qstar9537@naver.com>
2023-07-18 13:25:12 +0900

    :bento: arrow-narrow-up 아이콘 추가 (#82)

--- src/assets/icons/arrow/index.ts
+++ src/assets/icons/arrow/index.ts
@@ -3,3 +3,4 @@ export { default as ChevronDownIcon } from "./chevron-down.svg";
 export { default as ChevronLeftIcon } from "./chevron-left.svg";
 export { default as ArrowNarrowLeftIcon } from "./arrow-narrow-left.svg";
 export { default as ArrowNarrowTopIcon } from "./arrow-narrow-top.svg";
+export { default as ArrowNarrowUpIcon } from "./arrow-narrow-up.svg";
