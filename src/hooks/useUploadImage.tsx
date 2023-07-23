1b1ed130843dd2588dce0bb10b29b4a809c74a0d
cherryc0ck <qstar9537@naver.com>
2023-07-23 18:20:21 +0900

    :adhesive_bandage: 드래그 관련 텍스트 삭제 (#82)

--- src/hooks/useUploadImage.tsx
+++ src/hooks/useUploadImage.tsx
@@ -1,14 +1,16 @@
 import { useRef, useState } from "react";
 
 export default function useUploadImage() {
+  const [isLoading, setIsLoading] = useState(false);
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
   const [previewImage, setPreviewImage] = useState<string | null>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
 
     if (file) {
+      setIsLoading(true);
       setSelectedImage(file);
       const reader = new FileReader();
       reader.onloadend = () => {
@@ -16,9 +18,11 @@ export default function useUploadImage() {
       };
       reader.readAsDataURL(file);
     }
+    setIsLoading(false);
   };
 
   return {
+    isLoading,
     selectedImage,
     previewImage,
     fileInputRef,
