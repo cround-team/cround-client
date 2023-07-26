78790a17c2f9e65f5d22558aea06dd24f07807d9
cherryc0ck <qstar9537@naver.com>
2023-07-26 20:37:10 +0900

    :sparkles: 질문상세페이지 구현 (#82)

--- src/app/mypage/asked/[slug]/page.tsx
+++ src/app/mypage/asked/[slug]/page.tsx
@@ -3,6 +3,8 @@
 import MessageContainer from "@/components/domain/mypage/asked/container/MessageContainer";
 import useAskedDetail from "@/components/domain/mypage/asked/hooks/useAskedDetail";
 import * as S from "./styled";
+import { RefreshIcon } from "@/assets/icons";
+import { useAuthContext } from "@/context/AuthContext";
 
 type ContentDetailPageProps = {
   params: {
@@ -11,16 +13,27 @@ type ContentDetailPageProps = {
 };
 
 export default function MyAskedDetailPage({ params }: ContentDetailPageProps) {
-  const { messages, sender, receiver, nickname } = useAskedDetail(params.slug);
+  const { messages, sender, receiver, nickname, onFetchData } = useAskedDetail(
+    params.slug
+  );
+  const { user } = useAuthContext();
 
   return (
     <S.Section>
+      <S.MessageHeader
+        headerColor={user.roleName === "creator" ? "black" : "red"}
+      >
+        <span>
+          <strong>{nickname}</strong>님과 쪽지중
+        </span>
+      </S.MessageHeader>
       <MessageContainer
         messages={messages}
         memberId={params.slug}
         sender={sender}
         receiver={receiver}
         nickname={nickname}
+        onFetchData={onFetchData}
       />
     </S.Section>
   );
